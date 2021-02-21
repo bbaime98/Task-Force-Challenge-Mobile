65956a8c16887adbca0d677fd87d214368043b96
bien aimé <bybienaime@gmail.com>
2021-02-21 19:35:47 +0200

    setup the whole project
    - add a folder structure
    - add utils functions such as dimesions, platform and colors
    - setup testing config with jest
    - add sample test
    - add montserrat fonts
    - add splashscreena and related icons

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts  } from 'expo-font';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'semiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: 'bold'}}>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: 'semiBold'}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- utils/colors.js
+++ utils/colors.js
@@ -1,10 +1,9 @@
 export default {
-  primary: "#fc5c65",
-  secondary: "#4ecdc4",
-  black: "#000",
-  white: "#fff",
-  medium: "#6e6969",
-  light: "#f8f4f4",
+  primary: "#C1CF16",
+  secondary: "#495D69",
+  black: "#1C2834",
+  white: "#FFFFFF",
+  medium: "#F4F5F6",
+  dark: "#0C0D0D",
   danger: "#ff5252",
-  dark: "#0c0c0c"
 }
